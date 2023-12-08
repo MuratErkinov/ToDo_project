60d1f6d8471e73d6f9aa64b27c21a92bfb901ce2
Murat Erkinov <murat.erkinov1@gmail.com>
2023-12-08 12:16:05 +0600

    added todo model and in admin

--- homework/models.py
+++ homework/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    date_of_meeting = models.DateTimeField()
+    comment = models.TextField(null=True, blank=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+
+class Goal_for_month(models.Model):
+
+    MONTH_CHOICES = [
+        ("Январь", "Январь"),
+        ("Февраль", "Февраль"),
+        ("Март", "Март"),
+        ("Апрель", "Апрель"),
+        ("Май", "Май"),
+        ("Июнь", "Июнь"),
+        ("Июль", "Июль"),
+        ("Августь", "Августь"),
+        ("Сентябрь", "Сентябрь"),
+        ("Октябрь", "Октябрь"),
+        ("Ноябрь", "Ноябрь"),
+        ("Декабрь", "Декабрь")
+    ]
+
+    goal = models.CharField(max_length=250)
+    month = models.CharField(max_length=10, choices=MONTH_CHOICES, default="Январь")
+    difficulty = models.PositiveSmallIntegerField()
+    reason_for_goal = models.TextField()
